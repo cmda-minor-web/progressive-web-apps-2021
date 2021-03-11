631468199debbfde5c078c9e1ab1f4e26e41a975
joordy <jmfronik@gmail.com>
2021-03-11 22:24:38 +0100

    Reorganizing router files

--- routes/renders/home.js
+++ routes/renders/home.js
@@ -6,6 +6,7 @@ const home = async (req, res) => {
   try {
     let endpoint = `https://api.unsplash.com/photos/?client_id=${API_KEY}&per_page=33&order_by=popular`
     let response = await fetcher(endpoint)
+
     res.render('home', {
       data: response,
       style: 'home.css',
