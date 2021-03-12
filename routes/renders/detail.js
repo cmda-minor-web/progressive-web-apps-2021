d3930ffa2c62767050311fe34b7bb09eb8a8b475
joordy <jmfronik@gmail.com>
2021-03-12 12:31:31 +0100

    Fixed header and link

--- routes/renders/detail.js
+++ routes/renders/detail.js
@@ -7,16 +7,13 @@ const detail = async (req, res) => {
 
     let item = req._parsedUrl.path
     let query = item.replace('/image/', '')
-    console.log(query)
+
     const endpointOne = `https://api.unsplash.com/photos/${query}?client_id=${API_KEY}`
     const endpointTwo = `https://api.unsplash.com/photos/?client_id=${API_KEY}&per_page=10&order_by=popular`
 
-    console.log(item)
     const response = await fetcher(endpointOne)
     const recommendedResponse = await fetcher(endpointTwo)
 
-    console.log('response', response)
-    // console.log('recommendedResponse', recommendedResponse)
     res.render('details', {
       data: response,
       recommended: recommendedResponse,
