92caac2e7f1c0cea55ac1e99e832340c1b5439f0
NielsPeeters96 <78353674+NielsPeeters96@users.noreply.github.com>
2021-03-16 13:30:48 +0100

    Update

--- public/sw.js
+++ public/sw.js
@@ -13,26 +13,39 @@ const assets = [
 // installed event
 self.addEventListener("install", (event) => {
   console.log("install");
-
+  
   // pre-cache offline page and update cache automaticly
   event.waitUntil(
     caches.open("my-cache").then((cache) => {
       return cache.addAll(assets).then(() => self.skipWaiting());
     })
-  );
-});
-
-// activate event
-self.addEventListener("activate", () => {
-  console.log("active");
-});
-
-self.addEventListener("fetch", (event) => {
-  console.log("fetch event for: ", event.request.url);
-
-  event.respondWith(
-    fetch(event.request).catch((error) => {
-      return caches.open("my-cache").then((cache) => cache.match("/offline"));
+    );
+  });
+  
+  // activate event
+  self.addEventListener("activate", () => {
+    console.log("active");
+  });
+  
+  self.addEventListener("fetch", (event) => {
+    console.log("fetch event for: ", event.request.url);
+    
+    event.respondWith(
+      caches.open(CORE_CACHE_NAME).then(cache => {
+        return cache.match(event.request)
+        .then(response => {
+          if(response) {
+            return response
+          }
+          return fetch(event.request)
+          .then(response => {
+            cache.put(event.request, response.clone())
+            return response
+          })
+        }).catch((err) => {
+          return caches.open(CORE_CACHE_NAME).then(cache => cache.match('/offline'))
+        })
+      })
+      )
+      
     })
-  );
-});
